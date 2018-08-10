42045c1182cc5c8a763616074cb7eae582635525
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-08-10 14:44:49 +0300

    Integration Tests with Moxios

--- src/Root.js
+++ src/Root.js
@@ -2,24 +2,24 @@ import React from 'react';
 import reducers from 'reducers';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import reduxPromise from 'redux-promise';
 
-const initailState = {};
-const middleware = [];
+const initialState = {};
+const middleware = [reduxPromise];
 
-export default props => {
-  return (
-    <Provider
-      store={createStore(
-        reducers,
-        initailState,
-        compose(
-          applyMiddleware(...middleware),
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-      )}
-    >
-      {props.children}
-    </Provider>
+export default ({ children, initialState = {} }) => {
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(...middleware)
   );
+
+  return <Provider store={store}>{children}</Provider>;
 };
+
+// ,
+//         compose(
+//           applyMiddleware(...middleware),
+//           window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//             window.__REDUX_DEVTOOLS_EXTENSION__()
+//         )
